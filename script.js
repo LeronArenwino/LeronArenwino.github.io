213750f986e9ff10042895e1e90f0914b77fd1f4
Francisco Dueñas <frsduenasca@unal.edu.co>
2020-03-29 11:42:33 -0500

    File js was renamed and edited, new addEventListeners was added

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
       const burgerButton = document.querySelector('#burger-menu');
       console.log(burgerButton);
 
+      const portofolio = document.querySelector('#a-portofolio');
+      const events = document.querySelector('#a-events-list');
+      const contact = document.querySelector('#a-contact');
+
+      portofolio.addEventListener('click',hideShow)
+      events.addEventListener('click', hideShow)
+      contact.addEventListener('click', hideShow)
+
       burgerButton.addEventListener('click', hideShow)
 
       function hideShow() {

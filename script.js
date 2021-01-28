2314c9bd264e85169108f0e4fcaeca2cf4851cfb
Francisco Dueñas <frsduenasca@unal.edu.co>
2021-01-27 19:16:15 -0500

    Prettier format to file js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-      const menu = document.querySelector('.menu');
-      console.log(menu);
-      const burgerButton = document.querySelector('#burger-menu');
-      console.log(burgerButton);
+const menu = document.querySelector(".menu");
+console.log(menu);
+const burgerButton = document.querySelector("#burger-menu");
+console.log(burgerButton);
 
-      const portofolio = document.querySelector('#a-portofolio');
-      const events = document.querySelector('#a-events-list');
-      const contact = document.querySelector('#a-contact');
+const portofolio = document.querySelector("#a-portofolio");
+const events = document.querySelector("#a-events-list");
+const contact = document.querySelector("#a-contact");
 
-      portofolio.addEventListener('click',hideShow)
-      events.addEventListener('click', hideShow)
-      contact.addEventListener('click', hideShow)
+portofolio.addEventListener("click", hideShow);
+events.addEventListener("click", hideShow);
+contact.addEventListener("click", hideShow);
 
-      burgerButton.addEventListener('click', hideShow)
+burgerButton.addEventListener("click", hideShow);
 
-      function hideShow() {
-        if (menu.classList.contains('is-active')) {
-          menu.classList.remove('is-active');
-        } else {
-          menu.classList.add('is-active');
-        }
-      }
+function hideShow() {
+  if (menu.classList.contains("is-active")) {
+    menu.classList.remove("is-active");
+  } else {
+    menu.classList.add("is-active");
+  }
+}
